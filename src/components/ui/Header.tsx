ef3ff45dfb3496d35a2b12a36e50b8f6c3dbafa2
Mirhasankhan <mirhasan000034@gmail.com>
2024-06-28 15:37:15 +0600

    api chaned

--- src/components/ui/Header.tsx
+++ src/components/ui/Header.tsx
@@ -19,7 +19,7 @@ const Header = () => {
 
   return (
     <header className="">
-      <div className=" md:flex justify-between bg-gray-300 px-6 md:px-14 py-2">
+      <div className="md:flex justify-between bg-gray-300 px-6 md:px-14 py-2">
         <div className="flex gap-6">
           <h1>Welcome to Infinite Mart</h1>
           <div className="flex items-center gap-1">
